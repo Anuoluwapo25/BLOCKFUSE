40403e60b13f5b7e55b9111139ba09715e2b065c
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2025-02-20 16:01:45 +0100

    remove from liquidity implementation

--- week8/scripts/addLiqEth.ts
+++ week8/scripts/addLiqEth.ts
@@ -7,39 +7,36 @@ const IUniswapV2Pair = require('@uniswap/v2-core/build/IUniswapV2Pair.json');
 const addLiquidityETH = async () => {
    
     const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // Example token to pair with ETH
-    const theAddressWithTokenAndETH = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621"; // Address with both token and ETH
+    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const theAddressWithTokenAndETH = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621"; 
+    const factoryAddress ="0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
   
-    // Parameters for addLiquidityETH
-    const amountTokenDesired = ethers.parseUnits('1000', 18); // Example amount of DAI
+    const amountTokenDesired = ethers.parseUnits('1000', 18); 
     const amountTokenMin = ethers.parseUnits('990', 18);
-    const amountETHMin = ethers.parseEther('0.005'); // Minimum ETH amount
-    const ethAmount = ethers.parseEther('1'); // ETH to add as liquidity
+    const amountETHMin = ethers.parseEther('0.005'); 
+    const ethAmount = ethers.parseEther('1'); 
     const deadline = await helpers.time.latest() + 1000;
+    let AmtAMin = ethers.parseUnits('990', 6);
     
-    // Impersonate account
     await helpers.impersonateAccount(theAddressWithTokenAndETH);
     const impersonatedSigner = await ethers.getSigner(theAddressWithTokenAndETH);
     
-    // Get the contracts
     const tokenContract = await ethers.getContractAt('IERC20', DAIAddress);
     const uniswapContract = await ethers.getContractAt(
         'IUniswap', 
       UNIRouter
     );
     
-    // Check balances before
     const tokenBalBefore = await tokenContract.balanceOf(impersonatedSigner.address);
     const ethBalBefore = await ethers.provider.getBalance(impersonatedSigner.address);
     
     console.log('Token balance before:', ethers.formatUnits(tokenBalBefore, 18));
     console.log('ETH balance before:', ethers.formatEther(ethBalBefore));
     
-    // Approve the token for the router
     await tokenContract.connect(impersonatedSigner).approve(UNIRouter, amountTokenDesired);
     console.log('Token approved for router');
     
-    // Call addLiquidityETH
     console.log('-------------------------- Adding ETH liquidity -------------');
     const tx = await uniswapContract.connect(impersonatedSigner).addLiquidityETH(
       DAIAddress,
@@ -51,44 +48,57 @@ const addLiquidityETH = async () => {
       { value: ethAmount }
     );
     
-    // Wait for transaction to be mined
     const receipt = await tx.wait();
     console.log('-------------------------- ETH liquidity added -------------');
     
-    // Check balances after
     const tokenBalAfter = await tokenContract.balanceOf(impersonatedSigner.address);
     const ethBalAfter = await ethers.provider.getBalance(impersonatedSigner.address);
     
     console.log('Token balance after:', ethers.formatUnits(tokenBalAfter, 18));
     console.log('ETH balance after:', ethers.formatEther(ethBalAfter));
-    
-    // Get the LP token address (WETH pair)
-    const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-    const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // Wrapped ETH address on mainnet
-    
-    const factoryContract = await ethers.getContractAt(
-      IUniswapV2Factory.abi,
-      factoryAddress
-    );
-    
-    // TypeScript workaround
-    const lpTokenAddress = await (factoryContract as any).getPair(DAIAddress, WETH);
-    console.log("ETH-Token LP Token Address:", lpTokenAddress);
-    
-    // Get LP token balance
-    const lpToken = await ethers.getContractAt(IUniswapV2Pair.abi, lpTokenAddress);
-    const lpBalance = await lpToken.balanceOf(impersonatedSigner.address);
-    console.log("LP Token Balance:", ethers.formatUnits(lpBalance, 18));
-    
-    return {
-      lpTokenAddress,
+
+    const uniswapcontract = await ethers.getContractAt("IUniswap", UNIRouter)
+    const daicontact = await ethers.getContractAt("IERC20", DAIAddress)
+    await helpers.impersonateAccount(theAddressWithTokenAndETH);
+    const impersonateAccount = await ethers.getSigner(theAddressWithTokenAndETH);
+
+    const getfactoryContract = await ethers.getContractAt(IUniswapV2Factory.abi, factoryAddress)
+    const LPAddress = await (getfactoryContract as any).getPair(DAIAddress, WETH)
+    const lpTokenContract = await ethers.getContractAt(IUniswapV2Pair.abi, LPAddress);
+
+    const lpBalance = await lpTokenContract.balanceOf(impersonateAccount.address);
+
+    await lpTokenContract.connect(impersonateAccount).approve(UNIRouter, lpBalance);
+    console.log("LP Token Approved for Removal");
+
+    console.log("---------------Liquidity Removed Successfully!--------------");
+    console.log('Lp balance', ethers.formatUnits(lpBalance))
+ 
+    await uniswapcontract.connect(impersonateAccount).removeLiquidityETH(
+      DAIAddress ,
       lpBalance,
-      impersonatedSigner
-    };
+      AmtAMin,
+      amountETHMin,
+      impersonateAccount.address,
+      deadline,
+      )
+    
+
+
     
   };
+  
+  
 
-  addLiquidityETH().catch((error) => {
+
+
+
+addLiquidityETH().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
+
+//   main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
--- week8/scripts/uni.ts
+++ week8/scripts/uni.ts
@@ -26,8 +26,8 @@ const main = async () => {
     const usdcBal = await usdcContract.balanceOf(impersonatedSigner.address);
     const daiBal = await daiContract.balanceOf(impersonatedSigner.address);
     
-    console.log('impersonneted acct usdc bal BA:', ethers.formatUnits(usdcBal, 6));
-    console.log('impersonneted acct dai bal BA:', ethers.formatUnits(daiBal, 18));
+    console.log('impersonated acct usdc bal BA:', ethers.formatUnits(usdcBal, 6));
+    console.log('impersonated acct dai bal BA:', ethers.formatUnits(daiBal, 18));
     
     await usdcContract.connect(impersonatedSigner).approve(UNIRouter, AmtADesired);
     await daiContract.connect(impersonatedSigner).approve(UNIRouter, AmtBDesired);
@@ -48,8 +48,8 @@ const main = async () => {
     const usdcBalAfter = await usdcContract.balanceOf(impersonatedSigner.address);
     const daiBalAfter = await daiContract.balanceOf(impersonatedSigner.address);
     
-    console.log('impersonneted acct usdc bal AF:', ethers.formatUnits(usdcBalAfter, 6));
-    console.log('impersonneted acct dai bal AF:', ethers.formatUnits(daiBalAfter, 18));
+    console.log('impersonated acct usdc bal AF:', ethers.formatUnits(usdcBalAfter, 6));
+    console.log('impersonated acct dai bal AF:', ethers.formatUnits(daiBalAfter, 18));
     
     // Get LP token address
     const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
@@ -57,18 +57,14 @@ const main = async () => {
     const lpTokenAddress = await (factoryContract as any).getPair(USDCAddress, DAIAddress);
     console.log("LP Token Address:", lpTokenAddress);
     
-    // Create LP token contract instance
     const lpToken = await ethers.getContractAt(IUniswapV2Pair.abi, lpTokenAddress);
     
-    // Get LP token balance
     const lpBalance = await lpToken.balanceOf(impersonatedSigner.address);
     console.log("LP Token Balance:", ethers.formatUnits(lpBalance, 18));
     
-    // Step 6: Approve LP Token for Uniswap Router
     await lpToken.connect(impersonatedSigner).approve(UNIRouter, lpBalance);
     console.log("LP Token Approved for Removal");
     
-    // Step 7: Remove Liquidity
     console.log('---------------- Removing Liquidity ----------------');
     await uniswapContract.connect(impersonatedSigner).removeLiquidity(
         USDCAddress,
@@ -81,7 +77,6 @@ const main = async () => {
     );
     console.log('Liquidity Removed Successfully!');
     
-    // Step 8: Check Final Balances
     const usdcBalFinal = await usdcContract.balanceOf(impersonatedSigner.address);
     const daiBalFinal = await daiContract.balanceOf(impersonatedSigner.address);
     console.log('Final USDC Balance:', ethers.formatUnits(usdcBalFinal, 6));
@@ -93,76 +88,74 @@ const main = async () => {
     
 };
    
-// const addLiquidityETH = async () => {
-//     const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-//     const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-//     const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-//     const theAddressIFoundWithUSDCAndDAI = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+const addLiquidityETH = async () => {
+    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const theAddressIFoundWithUSDCAndDAI = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
     
-//     // Use correct interface
-//     const uniswapContract = await ethers.getContractAt('IUniswap', UNIRouter);
-//     const daiContract = await ethers.getContractAt('IERC20', DAIAddress);
+    const uniswapContract = await ethers.getContractAt('IUniswap', UNIRouter);
+    const daiContract = await ethers.getContractAt('IERC20', DAIAddress);
     
-//     // Use impersonated account
-//     const impersonatedSigner = await ethers.getSigner(theAddressIFoundWithUSDCAndDAI);
+    const impersonatedSigner = await ethers.getSigner(theAddressIFoundWithUSDCAndDAI);
     
-//     // Set ETH amount
-//     const ethAmount = ethers.parseEther('0.01'); // Small amount for testing
+    const ethAmount = ethers.parseEther('0.05'); 
     
-//     // Calculate equivalent token amount (assumes 1 ETH = 3000 DAI)
-//     const amountTokenDesired = ethers.parseUnits('3', 18); // 0.01 ETH * 3000 = 30 DAI
+   
+    const amountTokenDesired = ethers.parseUnits('30', 18); 
     
-//     // Set minimum amounts (99% of desired)
-//     const amountTokenMin = amountTokenDesired;
-//     const amountETHMin = ethAmount
+    const amountTokenMin = ethers.parseUnits('20', 18);
+    const amountETHMin = ethers.parseEther('0.001');
     
-//     // Check balances
-//     const ethBalance = await ethers.provider.getBalance(impersonatedSigner.address);
-//     const tokenBalance = await daiContract.balanceOf(impersonatedSigner.address);
+    const ethBalance = await ethers.provider.getBalance(impersonatedSigner.address);
+    const tokenBalance = await daiContract.balanceOf(impersonatedSigner.address);
     
-//     console.log('ETH Balance:', ethers.formatEther(ethBalance));
-//     console.log('DAI Balance:', ethers.formatUnits(tokenBalance, 18));
+    console.log('ETH Balance:', ethers.formatEther(ethBalance));
+    console.log('DAI Balance:', ethers.formatUnits(tokenBalance, 18));
     
-//     // Approve tokens
-//     await daiContract.connect(impersonatedSigner).approve(UNIRouter, amountTokenDesired);
+    const approved = await daiContract.connect(impersonatedSigner).approve(UNIRouter, amountTokenDesired);
+    // console.log("approve", approved)
     
-//     // Add liquidity
-//     const deadline = await helpers.time.latest() + 1000;
+    const deadline = await helpers.time.latest() + 1000;
     
-//     console.log('Adding liquidity with:');
-//     console.log('- DAI:', ethers.formatUnits(amountTokenDesired, 18));
-//     console.log('- ETH:', ethers.formatEther(ethAmount));
+    console.log('Adding liquidityeth with:');
+    console.log('- DAI:', ethers.formatUnits(amountTokenDesired, 18));
+    console.log('- ETH:', ethers.formatEther(ethAmount));
     
-//     const tx = await uniswapContract.connect(impersonatedSigner).addLiquidityETH(
-//       DAIAddress,
-//       amountTokenDesired,
-//       amountTokenMin,
-//       amountETHMin,
-//       impersonatedSigner.address,
-//       deadline,
-//       { value: ethAmount }
-//     );
+    const tx = await uniswapContract.connect(impersonatedSigner).addLiquidityETH(
+      DAIAddress,
+      amountTokenDesired,
+      amountTokenMin,
+      amountETHMin,
+      impersonatedSigner.address,
+      deadline,
+      { value: ethAmount }
+    );
     
-//     await tx.wait();
-//     console.log('Liquidity added successfully!');
+    await tx.wait();
+    console.log('Liquidityeth added successfully!');
     
-//     // Get LP token address
-//     const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-//     const factoryContract = await ethers.getContractAt(IUniswapV2Factory.abi, factoryAddress);
-//     const lpTokenAddress = await (factoryContract as any).getPair(DAIAddress, WETH);
+    const factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    const factoryContract = await ethers.getContractAt(IUniswapV2Factory.abi, factoryAddress);
+    const lpTokenAddress = await (factoryContract as any).getPair(DAIAddress, WETH);
     
-//     const lpToken = await ethers.getContractAt(IUniswapV2Pair.abi, lpTokenAddress);
-//     const lpBalance = await lpToken.balanceOf(impersonatedSigner.address);
+    const lpToken = await ethers.getContractAt(IUniswapV2Pair.abi, lpTokenAddress);
+    const lpBalance = await lpToken.balanceOf(impersonatedSigner.address);
     
-//     console.log('LP Token Address:', lpTokenAddress);
-//     console.log('LP Token Balance:', ethers.formatUnits(lpBalance, 18));
+    console.log('LP Token Address:', lpTokenAddress);
+    console.log('LP Token Balance:', ethers.formatUnits(lpBalance, 18));
     
-//     return { lpTokenAddress, lpBalance, impersonatedSigner };
-//   };
+    return { lpTokenAddress, lpBalance, impersonatedSigner };
+  };
     
 
   
 main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
+addLiquidityETH().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
